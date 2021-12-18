2c92be6508abd8ff8927bfb8196a229fbbe2263d
manshad <manshadchangampalli010@gmail.com>
2021-12-18 13:34:36 +0530

    the popup is added

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,5 @@
 const box = document.querySelectorAll(".boxs")
+const boxs = document.querySelectorAll(".boxss")
 
 box.forEach((item)=>{
     item.addEventListener("click",(e)=>{
@@ -7,3 +8,23 @@ box.forEach((item)=>{
         e.target.classList.add("active")
     })
 })
+boxs.forEach((item)=>{
+    item.addEventListener("click",(e)=>{
+       var current = document.querySelector(".boxss.active")
+       current.classList.remove("active");
+        e.target.classList.add("active")
+    })
+})
+
+
+setTimeout(()=>{
+    document.querySelector(".popup").style.display="grid"
+
+},3000)
+
+const closeButtonClicked = () =>{
+    document.querySelector(".popup").style.display="none"
+}
+const viewButtonClicked = () =>{
+    
+}
